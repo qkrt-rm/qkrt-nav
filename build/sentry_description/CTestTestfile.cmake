013977626c4751f612cb65dcf77629616a25e0ee
lukelpg <20lapg1@queensu.ca>
2025-11-29 15:56:21 -0500

    BT options added and some other logic but removed publisher to health topic so may not run rn

--- build/sentry_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/noah/autonav_ws/src/sentry_description
-# Build directory: /home/noah/autonav_ws/build/sentry_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
